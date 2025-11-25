13520c5e0368b95d05d602376d050437472a5dc8
vini-Zana <vinicius.zanotti@unesp.br>
2025-11-24 22:03:36 -0300

    Parte 3

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -11,5 +11,10 @@ public class Bullet : MonoBehaviour
 			print("hit " + collision.gameObject.name + "!");
 			Destroy(gameObject);
 		}
+		if (collision.gameObject.CompareTag("Wall"))
+		{
+			print("hit a wall" + collision.gameObject.name + "!");
+			Destroy(gameObject);
+		}
 	}
 }
